7055a3b41cfccd7e8c37607636be1120eb05b260
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2021-08-10 14:27:45 +0000

    fit content #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
 // import "bootswatch/dist/litera/bootstrap.min.css";
 // import "bootswatch/dist/litera/_variables.scss";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     NgbModule,
